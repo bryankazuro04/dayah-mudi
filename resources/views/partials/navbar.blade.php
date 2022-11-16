b4ad4cf4c93cbcb07a44e4c63043e02708a84215
yellaujg <yella.200180130@mhs.unimal.ac.id>
2022-11-16 16:58:02 +0700

    memisahkan/membuat partial navbar

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,59 @@
+<header>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-success">
+    <div class="container">
+      <a class="navbar-brand" href="#">Dayah Mudi</a>
+
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div
+        class="collapse navbar-collapse justify-content-end"
+        id="navbarNav"
+      >
+        <ul class="navbar-nav gap-2">
+          <li class="nav-item">
+            <div class="nav-link">Profile</div>
+
+            <div class="nav-item-content gap-1">
+              <a href="/identitas-dayah">Identitas</a>
+              <a href="/visi-misi">Visi & Misi</a>
+              <a href="/sejarah-dayah">Sejarah Singkat</a>
+              <a href="/struktur">Struktur Anggota Dayah</a>
+            </div>
+          </li>
+
+          <li class="nav-item">
+            <div class="nav-link">Features</div>
+
+            <div class="nav-item-content">
+              <a href="">a</a>
+              <a href="">a</a>
+              <a href="">a</a>
+              <a href="">a</a>
+            </div>
+          </li>
+
+          <li class="nav-item">
+            <div class="nav-link">Pricing</div>
+
+            <div class="nav-item-content">
+              <a href="">a</a>
+              <a href="">a</a>
+              <a href="">a</a>
+              <a href="">a</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</header>  
