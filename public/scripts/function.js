5c791cef68cb0c32d6ab591f9d1ae3b6f5ebc3a3
yellaujg <yella.200180130@mhs.unimal.ac.id>
2022-11-18 16:01:09 +0700

    membuat function dropdown nav item

--- public/scripts/function.js
+++ public/scripts/function.js
@@ -1,7 +1,17 @@
-const navLink = document.querySelectorAll('.nav-link'),
-  navItem = document.querySelector('.nav-item')
+const navLinks = document.querySelectorAll('.nav-link'),
+  navItems = document.getElementsByClassName('nav-item')
 
+function toggleNavItem() {
+  let itemClass = this.parentNode.className
 
-navLink.forEach(n => n.addEventListener('click', () => {
-  navItem.classList.toggle('open')
-}))
+  for (let i = 0; i < navItems.length; i++) {
+    navItems[i].className = 'nav-item'
+  }
+  if (itemClass === 'nav-item') {
+    this.parentNode.className = 'nav-item open'
+  }
+}
+
+navLinks.forEach((n) => {
+  n.addEventListener('click', toggleNavItem)
+})
