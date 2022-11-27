7690a3555e5e2fc66e6911cae3219816bb5686bb
Bryan <mhdalana211201@gmail.com>
2022-11-27 22:45:37 +0700

    membuat sidebar untuk berita

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,9 @@
+<aside class="ps-3">
+  @for ($i = 0; $i < 5; $i++)
+    <a href="#" class="d-flex" style="margin-bottom: 20px">
+      <img src="images/placeholder/placeholder-square.png" class="d-inline rounded-3" alt="">
+
+      <span class="ms-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, deserunt?</span>
+    </a>
+  @endfor
+</aside>
