1065825ace33d4cc250602642f6b2ea9336ed414
yellaujg <yella.200180130@mhs.unimal.ac.id>
2022-11-28 11:47:58 +0700

    menambahkan dump text dan including sidebar

--- resources/views/sejarah.blade.php
+++ resources/views/sejarah.blade.php
@@ -1,5 +1,10 @@
 @extends('template.template')
 
 @section('main')
-  <h1>Yella</h1>
+  <h1 class="my-4">Sejarah Dayah Mudi</h1>
+
+  <div class="row mb-5">
+    <div class="col-9 content-border"><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error vel repellat eum consequatur omnis in sint maxime odit perferendis amet fugit aspernatur ut asperiores blanditiis facere eius voluptatum neque minus nisi, veniam facilis deleniti et optio veritatis! Saepe, fugiat quisquam animi natus at voluptatem necessitatibus culpa deleniti magnam obcaecati ullam, hic odio, accusamus magni veritatis consequuntur sint tempore? Aspernatur reprehenderit minus quo illo alias itaque exercitationem consequuntur eos accusantium similique, atque laboriosam perspiciatis sequi dignissimos cumque adipisci ad quasi possimus corrupti! Accusamus et quasi placeat aut impedit rem deserunt odio voluptatem neque, eos, libero ea in veritatis architecto obcaecati? Non esse reiciendis provident accusamus quia porro a assumenda aperiam perspiciatis quidem iusto omnis officia hic eius nihil itaque, deserunt expedita dolores, obcaecati ullam possimus. Reprehenderit atque culpa, voluptates neque doloremque, impedit cum quo optio earum ab eveniet accusantium, magni fugiat quibusdam cupiditate molestias! Officiis placeat asperiores, perferendis amet odio assumenda aspernatur sit fuga debitis, odit doloribus illo eum laboriosam? Enim exercitationem quidem explicabo deleniti eos ut ducimus! Consequatur laudantium voluptas obcaecati quos accusantium, ut consectetur in incidunt eligendi, corporis enim deleniti perferendis? Maxime vel animi, in rerum eligendi ad porro eos? Tenetur, molestiae porro dolorum ratione totam nemo nostrum fuga?</p></div>
+    <div class="col-3">@include('partials.sidebar')</div>
+  </div>
 @endsection
--- resources/views/visi-misi.blade.php
+++ resources/views/visi-misi.blade.php
@@ -1,5 +1,15 @@
 @extends('template.template')
 
 @section('main')
-  <h1>Yeeeeeeee</h1>
+  <h1 class="my-4">Visi dan Misi Dayah Mudi</h1>
+
+  <div class="row mb-5">
+    <div class="col-9 content-border">
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore asperiores necessitatibus blanditiis commodi aspernatur, repudiandae voluptates laboriosam sint error odio beatae culpa quo consequatur aperiam nam nostrum dolorem voluptate cupiditate, exercitationem atque iure labore. Consectetur, inventore! Ipsum sapiente laboriosam deleniti, hic quas, ut asperiores nostrum ad delectus minus nam id inventore voluptatum recusandae dignissimos, obcaecati possimus quidem necessitatibus ducimus harum voluptate magnam autem optio? Nulla unde, aliquam esse aspernatur magni debitis eveniet fuga ratione placeat maxime et itaque repudiandae ipsam fugiat consectetur libero. Possimus quisquam modi enim et facilis. Quia facilis provident officia, veritatis sint blanditiis illum, quos necessitatibus nam molestias impedit quibusdam sapiente, atque porro hic! Atque, laboriosam voluptas! At autem, deserunt aliquid obcaecati quae eveniet accusamus iusto magnam. Laboriosam accusamus corporis vitae dignissimos quod itaque reiciendis porro sit veritatis! Consectetur ex, adipisci corporis eligendi quae earum voluptatibus ipsam omnis culpa eveniet corrupti, repudiandae, tempora non et illum. Asperiores id omnis, pariatur quas veritatis, maxime itaque ipsam quibusdam nulla nihil doloribus alias repudiandae natus quaerat debitis reiciendis aspernatur. Veniam corrupti ullam voluptatem dignissimos illo molestiae! Sapiente quis nemo dicta dolores repellendus aut voluptatem molestias. Nam id accusantium quia corrupti quod iusto sapiente itaque necessitatibus harum corporis, maxime sit veniam!</p>
+    </div>
+
+    <div class="col-3">
+      @include('partials.sidebar')
+    </div>
+  </div>
 @endsection
