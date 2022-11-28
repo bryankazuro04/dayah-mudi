8492ce57ff8b1665693c43a8990ca0970592d1bb
yellaujg <yella.200180130@mhs.unimal.ac.id>
2022-11-28 16:26:13 +0700

    membuat model berita & function berita pada controller

--- app/Http/Controllers/DayahController.php
+++ app/Http/Controllers/DayahController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\BeritaModel;
 use Illuminate\Http\Request;
 
 class DayahController extends Controller
@@ -29,5 +30,13 @@ class DayahController extends Controller
     public function identitas()
     {
       return view('identitas', ['title' => 'Identitas']);
+    }
+
+    public function berita()
+    {
+      return view('berita', [
+        'title' => 'Berita',
+        'news' => BeritaModel::all(),
+      ]);
     }
 }
