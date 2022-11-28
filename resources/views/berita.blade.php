41dabab398d904d4d6546444e9544a15c959dfb4
Bryan <mhdalana211201@gmail.com>
2022-11-29 00:36:30 +0700

    mengatur berita berurut dari yang terbaru

--- resources/views/berita.blade.php
+++ resources/views/berita.blade.php
@@ -4,12 +4,12 @@
     <h1 class="my-3">Berita</h1>
 
     <div class="row">
-      @foreach ($news as $berita)
+      @foreach ($berita as $news)
         <div class="col-md-3 mb-4">
           <div class="card" >
             <div class="card-body">
-              <h5 class="card-title">{{ $berita->judul }}</h5>
-              <p class="card-text">{{ $berita->isi_berita }}</p>
+              <h5 class="card-title">{{ $news->judul }}</h5>
+              <p class="card-text">{{ $news->isi_berita }}</p>
             </div>
           </div>
         </div>
--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,9 +1,9 @@
 <aside class="ps-3">
-  @for ($i = 0; $i < 5; $i++)
+  @foreach ($news as $n)
     <a href="#" class="d-flex" style="margin-bottom: 20px">
       <img src="images/placeholder/placeholder-square.png" class="d-inline rounded-3" width="100" alt="">
 
-      <span class="ms-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, deserunt?</span>
+      <span class="ms-2">{{ $n->judul }}</span>
     </a>
-  @endfor
+  @endforeach
 </aside>
