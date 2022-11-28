02521b088a5e23d2f929de922695d91e7d81f4bf
Bryan <mhdalana211201@gmail.com>
2022-11-29 00:35:06 +0700

    mengambil data dari database untuk dikirim di view

--- app/Http/Controllers/DayahController.php
+++ app/Http/Controllers/DayahController.php
@@ -7,36 +7,57 @@ use Illuminate\Http\Request;
 
 class DayahController extends Controller
 {
-    public function home()
-    {
-        return view('home', ['title' => 'Home']);
-    }
+  public function __construct()
+  {
+    $this->berita = BeritaModel::latest()->limit(5)->get();
+  }
+  
+  public function home()
+  {
+    return view('home', [
+      'title'   => 'Home',
+      'news'    => $this->berita,
+      'berita'  => BeritaModel::latest()->limit(15)->get()
+    ]);
+  }
 
-    public function visiMisi()
-    {
-      return view('visi-misi', ['title' => 'Visi & Misi']);
-    }
+  public function visiMisi()
+  {
+    return view('visi-misi', [
+      'title' => 'Visi & Misi',
+      'news'  => $this->berita
+    ]);
+  }
 
-    public function struktur()
-    {
-      return view('struktur', ['title' => 'Struktur Organisasi']);
-    }
+  public function struktur()
+  {
+    return view('struktur', [
+      'title' => 'Struktur Organisasi',
+      'news'  => $this->berita
+    ]);
+  }
 
-    public function sejarah()
-    {
-      return view('sejarah', ['title' => 'Sejarah']);
-    }
+  public function sejarah()
+  {
+    return view('sejarah', [
+      'title' => 'Sejarah',
+      'news'  => $this->berita
+    ]);
+  }
 
-    public function identitas()
-    {
-      return view('identitas', ['title' => 'Identitas']);
-    }
+  public function identitas()
+  {
+    return view('identitas', [
+      'title' => 'Identitas',
+      'news'  => $this->berita
+    ]);
+  }
 
-    public function berita()
-    {
-      return view('berita', [
-        'title' => 'Berita',
-        'news' => BeritaModel::all(),
-      ]);
-    }
+  public function berita()
+  {
+    return view('berita', [
+      'title'   => 'Berita',
+      'berita'  => BeritaModel::all(),
+    ]);
+  }
 }
