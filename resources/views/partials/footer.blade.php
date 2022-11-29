03d47d489344d8ca00adb682d3ae37f8b1fdff6b
yellaujg <yella.200180130@mhs.unimal.ac.id>
2022-11-29 15:00:37 +0700

    membuat footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="bg-secondary bg-opacity-25 px-4 pt-4">
+  <div class="container-fluid px-5">
+    <img src="../images/logo/logo-dayah.png" alt="Dayah Mudi" style="width: 10rem">
+    <div class="copyright mt-4 pb-2"><span>&copy All Rights by KKNT 02</span></div>
+  </div>
+</footer>
