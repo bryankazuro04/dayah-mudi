faa855842049f65784f77a06a061cab84269607c
yellaujg <yella.200180130@mhs.unimal.ac.id>
2022-12-01 11:50:04 +0700

    mengganti template halaman dan menambah searchbar

--- resources/views/admin/data-santri.blade.php
+++ resources/views/admin/data-santri.blade.php
@@ -1,9 +1,18 @@
-@extends('template.template')
+@extends('admin.dashboard')
 
 @section('main')
     <h1 class="mb-4">Data Santri</h1>
 
-    <table class="table table-hover text-center" style="table-layout: fixed">
+    <div class="input-group w-25 mb-3">
+      <label for="inputSantri" class="input-group-text"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+      </svg></label>
+      <input type="search" id="inputSantri" onkeyup="searchSantri()" placeholder="Cari nama santri" class="form-control">
+    </div>
+    
+    <a href="/admin/penambahan-data-santri" role="button" class="btn btn-primary mb-2">Tambah Santri</a>
+
+    <table class="table table-hover text-center" id="tableSantri" style="table-layout: fixed">
       <thead class="table-success">
         <tr>
           <th scope="col">No.</th>
@@ -31,4 +40,8 @@
         @endforeach
       </tbody>
     </table>
+@endsection
+
+@section('javascript')
+    <script src="../scripts/table.js"></script>
 @endsection
