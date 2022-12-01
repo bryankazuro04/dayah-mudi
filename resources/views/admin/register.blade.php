d49b39592934c5b5471c11efa088b144429d3e75
yellaujg <yella.200180130@mhs.unimal.ac.id>
2022-12-01 11:48:52 +0700

    membuat validation untuk registrasi

--- resources/views/admin/register.blade.php
+++ resources/views/admin/register.blade.php
@@ -5,20 +5,39 @@
     <div class="col-lg-5">
       <h1 class="my-4 text-center">Register</h1>
 
-      <form action="">
+      <form action="/register" method="post">
+        @csrf
         <section class="form-floating mb-3">
-          <input type="name" name="name" class="form-control" id="floatingInput" placeholder="Name Example">
-          <label for="floatingInput">Email address</label>
+          <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" id="name" placeholder="Name Example">
+          <label for="name">Name</label>
+
+          @error('name')
+              <div class="invalid-feedback">
+                {{ $message }}
+              </div>
+          @enderror
         </section>
 
         <section class="form-floating mb-3">
-          <input type="email" name="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-          <label for="floatingInput">Email address</label>
+          <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com">
+          <label for="email">Email address</label>
+
+          @error('email')
+              <div class="invalid-feedback">
+                {{ $message }}
+              </div>
+          @enderror
         </section>
 
         <section class="form-floating">
-          <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Password">
-          <label for="floatingPassword">Password</label>
+          <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" id="password" placeholder="Password">
+          <label for="password">Password</label>
+
+          @error('password')
+              <div class="invalid-feedback">
+                {{ $message }}
+              </div>
+          @enderror
         </section>
 
         <button type="submit" class="btn btn-primary mt-3">Register</button>
