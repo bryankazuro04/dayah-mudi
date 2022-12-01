c14a8399deb7ed0e1cf4e0c02a9ca5188efcfffc
yellaujg <yella.200180130@mhs.unimal.ac.id>
2022-12-01 11:48:15 +0700

    membuat validation untuk login

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -3,17 +3,44 @@
 @section('main')
   <div class="row justify-content-center">
     <h1 class="my-4 text-center">Login</h1>
-
+    
     <div class="col-lg-5">
-      <form action="">
+      @if (session()->has('status'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+          {{ session('status') }}
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+      @endif
+
+      @if (session()->has('loginError'))
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+          {{ session('status') }}
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+      @endif
+
+      <form action="/login" method="post">
+        @csrf
         <section class="form-floating mb-3">
-          <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-          <label for="floatingInput">Email address</label>
+          <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com">
+          <label for="email">Email address</label>
+
+          @error('email')
+              <div class="invalid-feedback">
+                {{ $message }}
+              </div>
+          @enderror
         </section>
 
         <section class="form-floating">
-          <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-          <label for="floatingPassword">Password</label>
+          <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" id="password" placeholder="Password">
+          <label for="password">Password</label>
+
+          @error('password')
+              <div class="invalid-feedback">
+                {{ $message }}
+              </div>
+          @enderror
         </section>
 
         <button type="submit" class="btn btn-primary mt-3">Login</button>
