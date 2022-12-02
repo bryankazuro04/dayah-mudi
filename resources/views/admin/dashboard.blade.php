c5c12912d23b00203634003aa461033e937c6fc2
yellaujg <yella.200180130@mhs.unimal.ac.id>
2022-12-02 15:38:13 +0700

    menambahkan link style & function

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,12 +8,12 @@
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
       
-    @vite(['public/styles/style.css', 'public/scripts/function.js'])
+    {{-- @vite(['public/styles/style.css', 'public/scripts/function.js']) --}}
 
     <link rel="shortcut icon" href="../images/logo/logo-dayah.png" type="image/x-icon">
 
-    
-    
+    <link rel="stylesheet" href="../styles/style.css">
+
     <title>Dayah Mudi | {{ $title }}</title>
   </head>
 
@@ -32,6 +32,8 @@
     
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+    <script src="../scripts/function.js" type="module"></script>
 
     @yield('javascript')
   </body>
