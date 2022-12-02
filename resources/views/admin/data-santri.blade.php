c4c0098db10bff43a0684600241259a6194bcc16
yellaujg <yella.200180130@mhs.unimal.ac.id>
2022-12-02 11:49:08 +0700

    menambahkan class table responsive

--- resources/views/admin/data-santri.blade.php
+++ resources/views/admin/data-santri.blade.php
@@ -12,34 +12,36 @@
     
     <a href="/admin/penambahan-data-santri" role="button" class="btn btn-primary mb-2">Tambah Santri</a>
 
-    <table class="table table-hover text-center" id="tableSantri" style="table-layout: fixed">
-      <thead class="table-success">
-        <tr>
-          <th scope="col">No.</th>
-          <th scope="col">Nama Santri</th>
-          <th scope="col">No. Induk</th>
-          <th scope="col">Tempat Lahir</th>
-          <th scope="col">Tanggal Lahir</th>
-          <th scope="col">Nama Orang Tua</th>
-          <th scope="col" class="text-center">Alamat</th>
-        </tr>
-      </thead>
-
-      <tbody>
-        <?php $i = 1 ?>
-        @foreach ($santri as $s)
+    <div class="table-responsive">
+      <table class="table table-hover text-center" id="tableSantri" style="table-layout: fixed">
+        <thead class="table-success">
           <tr>
-            <th scope="row">{{  $i++; }}</th>
-            <td>{{ $s->nama_santri }}</td>
-            <td>{{ $s->no_induk }}</td>
-            <td>{{ $s->tempat_lahir }}</td>
-            <td>{{ $s->tanggal_lahir }}</td>
-            <td>{{ $s->nama_ortu }}</td>
-            <td>{{ $s->alamat }}</td>
+            <th scope="col">No.</th>
+            <th scope="col">Nama Santri</th>
+            <th scope="col">No. Induk</th>
+            <th scope="col">Tempat Lahir</th>
+            <th scope="col">Tanggal Lahir</th>
+            <th scope="col">Nama Orang Tua</th>
+            <th scope="col" class="text-center">Alamat</th>
           </tr>
-        @endforeach
-      </tbody>
-    </table>
+        </thead>
+
+        <tbody>
+          <?php $i = 1 ?>
+          @foreach ($santri as $s)
+            <tr>
+              <th scope="row">{{  $i++; }}</th>
+              <td>{{ $s->nama_santri }}</td>
+              <td>{{ $s->no_induk }}</td>
+              <td>{{ $s->tempat_lahir }}</td>
+              <td>{{ $s->tanggal_lahir }}</td>
+              <td>{{ $s->nama_ortu }}</td>
+              <td>{{ $s->alamat }}</td>
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
+    </div>
 @endsection
 
 @section('javascript')
