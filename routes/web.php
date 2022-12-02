2200e333aa3bb7c383ad157178b2cee02c6f0a91
yellaujg <yella.200180130@mhs.unimal.ac.id>
2022-12-02 15:42:56 +0700

    menambah middleware & menambah route berita

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Http\Controllers\BeritaController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\DayahController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SantriController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,13 +24,15 @@ Route::get('/visi-misi', [DayahController::class, 'visiMisi']);
 Route::get('/struktur', [DayahController::class, 'struktur']);
 Route::get('/sejarah-dayah', [DayahController::class, 'sejarah']);
 Route::get('/identitas-dayah', [DayahController::class, 'identitas']);
-Route::get('/admin/data-santri', [SantriController::class, 'index']);
+Route::get('/berita', [BeritaController::class, 'berita']);
+Route::get('/berita/{berita}', [BeritaController::class, 'tampilBerita']);
 Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
 Route::post('/login', [LoginController::class, 'authenticate']);
 Route::post('/logout', [LoginController::class, 'logout']);
 Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
 Route::get('/admin/dashboard', [AdminController::class, 'index'])->middleware('auth');
-Route::get('/admin/penambahan-data-santri', [SantriController::class, 'penambahanDataSantri']);
+Route::get('/admin/data-santri', [SantriController::class, 'index'])->middleware('auth');
+Route::get('/admin/penambahan-data-santri', [SantriController::class, 'penambahanDataSantri'])->middleware('auth');
 Route::post('/admin/penambahan-data-santri', [SantriController::class, 'store']);
-Route::get('/berita', [DayahController::class, 'berita']);
+Route::resource('/admin/dashboard/berita', BeritaController::class)->middleware('auth');
