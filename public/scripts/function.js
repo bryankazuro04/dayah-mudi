e5bb8d303d8ccb84b626b67dbfbb05c78c98af87
Bryan <mhdalana211201@gmail.com>
2022-12-04 22:25:44 +0700

    added direct import fetch & trix js

--- public/scripts/function.js
+++ public/scripts/function.js
@@ -1,17 +1,22 @@
-const navLinks = document.querySelectorAll('.nav-link'),
-  navItems = document.getElementsByClassName('nav-item')
+import "./trix.umd.min.js";
+import "./fetch.js";
+
+const navLinks = document.querySelectorAll(".nav-link"),
+  navItems = document.getElementsByClassName("nav-item");
 
 function toggleNavItem() {
-  let itemClass = this.parentNode.className
+  let itemClass = this.parentNode.className;
 
   for (let i = 0; i < navItems.length; i++) {
-    navItems[i].className = 'nav-item'
+    navItems[i].className = "nav-item";
   }
-  if (itemClass === 'nav-item') {
-    this.parentNode.className = 'nav-item open'
+  if (itemClass === "nav-item") {
+    this.parentNode.className = "nav-item open";
   }
 }
 
 navLinks.forEach((n) => {
-  n.addEventListener('click', toggleNavItem)
-})
+  n.addEventListener("click", toggleNavItem);
+});
+
+document.addEventListener("trix-file-accept", (e) => e.preventDefault());
