9a35cb6b581bfda32c111c0e7a17592523661e9b
Bryan <mhdalana211201@gmail.com>
2022-12-04 22:31:02 +0700

    added image & time

--- resources/views/berita.blade.php
+++ resources/views/berita.blade.php
@@ -8,9 +8,13 @@
         <div class="col-md-3 mb-4">
           <article class="card" >
             <div class="card-body">
+              <img src="https://source.unsplash.com/900x600/?personal" alt="" class="card-img-top">
+
               <h5 class="card-title">
                 <a href="/berita/{{ $news->slug }}">{{ $news->judul }}</a>
               </h5>
+
+              <p class="card-text mb-1"><small class="text-muted">{{ $news->created_at->diffForHumans() }}</small></p>
               <p class="card-text">{{ $news->isi_berita }}</p>
             </div>
           </article>
