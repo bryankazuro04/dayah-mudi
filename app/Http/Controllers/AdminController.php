e10af41a604c2db522c5b56e2ff576bdae058bbf
Bryan <mhdalana211201@gmail.com>
2022-12-08 00:19:14 +0700

    querying model berita & santri

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,14 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SantriModel;
+use App\Models\Berita;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function index()
     {
-        return view('template.dashboard', [
-            'title' => 'Dashboard'
+        return view('admin.index', [
+            'title' => 'Dashboard',
+            'santri' => SantriModel::latest()->limit(3)->get(),
+            'berita' => Berita::latest()->limit(3)->get()
         ]);
     }
 }
