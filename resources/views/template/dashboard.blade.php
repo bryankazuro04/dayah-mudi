a0a1ed5bf1221ad14700bdac64927b764c6342d4
Bryan <mhdalana211201@gmail.com>
2022-12-26 08:40:22 +0700

    memperbaiki source assets

--- resources/views/template/dashboard.blade.php
+++ resources/views/template/dashboard.blade.php
@@ -8,13 +8,11 @@
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
       
-    {{-- @vite(['public/styles/style.css', "public/styles/trix.css", 'public/scripts/function.js']) --}}
-    <link rel="stylesheet" type="text/css" href="https://unpkg.com/trix@2.0.0/dist/trix.css">
-    <script type="text/javascript" src="https://unpkg.com/trix@2.0.0/dist/trix.umd.min.js"></script>
+    @vite()
 
-    <link rel="shortcut icon" href="../images/logo/logo-dayah.png" type="image/x-icon">
-
-    <link rel="stylesheet" href="../styles/style.css">
+    <link rel="shortcut icon" href="/images/logo/logo-dayah.png" type="image/x-icon">
+    <link rel="stylesheet" href="/styles/style.css">
+    <link rel="stylesheet" href="/styles/trix.css">
 
     <title>Dayah Mudi | {{ $title }}</title>
   </head>
@@ -35,8 +33,7 @@
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 
-    <script src="../scripts/function.js" type="module"></script>
-
     @yield('javascript')
+    <script src="/scripts/function.js" type="module"></script>
   </body>
 </html>
--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -1,22 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <!-- Required Meta Tag -->
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  
-  <!-- CSS only -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-  
-  <link rel="stylesheet" href="../styles/style.css">
-  
-  {{-- @vite(['public/styles/style.css', 'public/scripts/function.js']) --}}
+  <head>
+    <!-- Required Meta Tag -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    
+    @vite()
+    
+    <link rel="shortcut icon" href="/images/logo/logo-dayah.png" type="image/x-icon">
+    <link rel="stylesheet" href="/styles/style.css">
 
-  <link rel="shortcut icon" href="../images/logo/logo-dayah.png" type="image/x-icon">
+    <title>Dayah Mudi | {{ $title }}</title>
+  </head>
 
-  <title>Dayah Mudi | {{ $title }}</title>
-</head>
   <body>
     @include('partials.navbar')
       
@@ -28,11 +28,10 @@
 
     @include('partials.footer')
 
-    
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     
     @yield('javascript')
-    <script src="../scripts/function.js" type="module"></script>
+    <script src="/scripts/function.js" type="module"></script>
   </body>
 </html>
